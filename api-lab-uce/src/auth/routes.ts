3447431fa2028bb2a5d3ff6dfe96d0e598c5434b
Dennis290699 <trujilloadrian29@gmail.com>
2026-03-26 13:52:26 -0500

    Update routes.ts

--- api-lab-uce/src/auth/routes.ts
+++ api-lab-uce/src/auth/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { login, register } from './controller';
+import { login, adminLogin, register } from './controller';
 
 const router = Router();
 
 router.post('/login', login);
+router.post('/admin-login', adminLogin);
 router.post('/register', register); // Optional, for initial setup
 
 export default router;
